d13328f682d501bfc83410337c4ac05b935a6ec0
BuildTools <unconfigured@null.spigotmc.org>
2023-01-19 20:52:22 +0100

    Initial Commit

--- src/main/java/net/davido152/toovisegradforyou/TooVisegradForYou.java
+++ src/main/java/net/davido152/toovisegradforyou/TooVisegradForYou.java
@@ -1,8 +1,10 @@
 package net.davido152.toovisegradforyou;
 
 import com.mojang.logging.LogUtils;
+import net.davido152.toovisegradforyou.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,16 +12,17 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TooVisegradForYou.MOD_ID)
-public class TooVisegradForYou
-{
+public class TooVisegradForYou {
     public static final String MOD_ID = "toovisegradforyou";
 
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    public TooVisegradForYou()
-    {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    public TooVisegradForYou() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
