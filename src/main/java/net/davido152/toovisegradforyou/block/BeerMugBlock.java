b8335ca5739c7e491711951388b58dff82712ad1
BuildTools <unconfigured@null.spigotmc.org>
2023-04-23 16:12:20 +0200

    Made placed Beer Mugs drinkable

--- src/main/java/net/davido152/toovisegradforyou/block/BeerMugBlock.java
+++ src/main/java/net/davido152/toovisegradforyou/block/BeerMugBlock.java
@@ -1,11 +1,14 @@
 package net.davido152.toovisegradforyou.block;
 
+import net.davido152.toovisegradforyou.item.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
@@ -34,8 +37,8 @@ public class BeerMugBlock extends Block {
         ItemStack beerMug = new ItemStack(pState.getBlock());
 
         PlayerInfo playerinfo = Minecraft.getInstance().getConnection().getPlayerInfo(pPlayer.getGameProfile().getId());
-        if(playerinfo != null && playerinfo.getGameMode() == GameType.ADVENTURE) {
-          return super.use(pState, pLevel, pPos, pPlayer, pHand ,pHit);
+        if (playerinfo != null && playerinfo.getGameMode() == GameType.ADVENTURE) {
+            return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
         } else if(pPlayer.isShiftKeyDown() && pPlayer.isCreative()) {
             if(pPlayer.getInventory().findSlotMatchingItem(beerMug) == -1) {
                 pPlayer.getInventory().add(beerMug);
@@ -48,7 +51,14 @@ public class BeerMugBlock extends Block {
             pLevel.removeBlock(pPos, false);
             pLevel.playSound(null, pPos, soundType.getBreakSound(), SoundSource.BLOCKS, (soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
             return InteractionResult.SUCCESS;
-        } else {
+        }  else if(pState.is(ModBlocks.FULL_BEER_MUG.get())) {
+            pLevel.setBlockAndUpdate(pPos, ModBlocks.EMPTY_BEER_MUG.get().defaultBlockState());
+            pLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), pPlayer.getEatingSound(new ItemStack(ModItems.FULL_BEER_MUG.get())), SoundSource.NEUTRAL, 1.0F, pPlayer.level.random.nextFloat() * 0.1F + 0.9F);
+            pPlayer.getFoodData().eat(beerMug.getItem(), beerMug, pPlayer);
+            pPlayer.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 150));
+            return InteractionResult.SUCCESS;
+        }
+        else {
             return super.use(pState, pLevel, pPos, pPlayer, pHand ,pHit);
         }
     }
--- src/main/java/net/davido152/toovisegradforyou/item/FullBeerMugItem.java
+++ src/main/java/net/davido152/toovisegradforyou/item/FullBeerMugItem.java
@@ -1,8 +1,11 @@
 package net.davido152.toovisegradforyou.item;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 
@@ -19,5 +22,14 @@ public class FullBeerMugItem extends BeerMugItem {
             ((Player) pLivingEntity).getInventory().add(beerMug);
         }
         return super.finishUsingItem(pStack, pLevel, pLivingEntity);
+    }
+
+    public UseAnim getUseAnimation(ItemStack pStack) {
+        return UseAnim.DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatingSound() {
+        return SoundEvents.GENERIC_DRINK;
     }
 }
